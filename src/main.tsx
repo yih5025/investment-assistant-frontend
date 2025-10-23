8308aa968a5a620ab663766459cb1d12bef42302
yih5025 <yih5025@naver.com>
2025-10-23 16:41:15 +0900

    frontend: update 2025-10-23 16:41:15

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // 다크 모드와 라이트 모드 CSS를 모두 로드 (data-theme 속성으로 전환)
+import './styles/globals.css'
+
 import './styles/globals-dark.css'
 import './styles/globals-light.css'
 import App from './App.tsx'
